d8a4505dc078997b882672ddc84542a2e47b16f0
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-27 19:14:25 -0400

    commit tree cb28c7752d645b24e08a805895038c1ab57722e6

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+  return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(array) {
+  return array.slice(-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fare) {
+  return function(multiplier) {
+    return fare * multiplier
+  }
+}
+
+let fareDoubler = function(fare) {
+  return createFareMultiplier(fare)(2)
+}
+
+let fareTripler = function(fare) {
+  return createFareMultiplier(fare)(3)
+}
+
+function selectDifferentDrivers(drivers, func) {
+  return func(drivers)
+}
